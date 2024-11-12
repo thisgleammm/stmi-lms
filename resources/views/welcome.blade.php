ef707af4199fa749eaa96f8d1ec6efb7fb95410f
thisgleammm <thisgleam@gmail.com>
2024-11-12 23:52:12 +0700

    Install AlpineJS

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         @vite('resources/css/app.css')
+        <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
         <title>Laravel</title>
 
