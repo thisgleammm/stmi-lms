5e5614b0828a7a6690a40a29c0359787f9376076
thisgleammm <thisgleam@gmail.com>
2024-11-27 17:35:08 +0700

    refactor: remove dark mode, modify recaptcha, add icon, change profile menu

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,38 +2,61 @@
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
+    <form class="space-y-2" method="POST" action="{{ route('login') }}">
         @csrf
-
+        <div class="text-center mb-3 flex justify-center">
+            <div class="image size-20 mb-3">
+                <img src="{{ url('/images/LogoApps.svg') }}" alt="Logo" class="max-w-full h-auto">
+            </div>
+        </div>
+        <div class="text-center mb-3 flex justify-center">
+            <div class="image mb-3">
+                <img src="{{ url('/images/titleLogo.svg') }}" alt="Title" class="max-w-full h-auto">
+            </div>
+        </div>
         <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+        <div class="pb-2">
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" placeholder="Enter Your Email" />
+
+            <div class="mt-4">
+                <x-text-input id="password" class="block mt-1 w-full"
+                                type="password"
+                                name="password"
+                                required autocomplete="current-password"
+                                minlength="8"
+                                placeholder="Enter Your Password" />
+
+                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            </div>
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
+        <div class="flex items-start">
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+                <a class="ms-2 underline text-sm text-gray-400 hover:text-gray-900" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
+        </div>
 
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
+        {{-- underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 --}}
+        
+        <div class="mb-3">
+            {!! htmlFormSnippet() !!}
+
+            @if($errors->has('g-recaptcha-response'))
+
+            <x-input-error :messages="$errors->first('g-recaptcha-response')" class="mt-2" />
+
+            @endif
+        </div>
+
+        <x-primary-button class="text-center mb-3 flex justify-center">
+            {{ __('LOGIN') }}
+        </x-primary-button>
+
+        <div class="footer pt-4 text-center">
+            <img src="{{ url('/images/footerLogo.svg') }}" alt="Footer Logo" class="max-w-full h-auto">
         </div>
     </form>
 </x-guest-layout>
