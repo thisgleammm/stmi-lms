e92f3733871a1d3f66bf7808c63efcbc94b2e9c6
peopleacc <accpeopleacc@gmail.com>
2024-12-03 12:12:29 +0700

    create footer.blade.php

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<footer>
+            <div class="pt-64 ">
+                <div class="max-w-7xl  ">
+                    <div class="bg-regal-blue flex justify-center static py-12 text-white">
+
+                        <a href="" class="px-10 pt-14">
+                        <img src="{{ url('/images/LogoApps.svg') }}" alt=""class="mt-4 ml-8 mb-4 w-20 ">   
+                        <div class="justify-center text-center mr-4 text-xs ">
+                            <p>STMI Interactive Management</p>
+                            <p>Platform For E-Learning</p>
+                        </div>
+                        </a>  
+
+                        <div class="flex px-10 mr-16 text-xs">
+                            <div class="mt-4 mr-10">
+                                <p class="mb-3">Address</p>
+                                <p>JL. Letjen Suprapto No.26, Cemp. Putih</p>
+                                <p>Tim., Kec Cemp. Putih, Kota Jakarta Pusat,</p>
+                                <p>Daerah Khusus Ibu Kota Jakarta 105010</p>
+                                <div class="mt-3">
+                                <iframe src=   "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15866.81479988534!2d106.8678534!3d-6.1704146!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f5043973ff63%3A0xc125c1242e567fd1!2sPolytechnic%20STMI%20Jakarta!5e0!3m2!1sen!2sid!4v1709964453699!5m2!1sen!2sid" frameborder="0"></iframe>
+                                </div>
+                            </div>
+                        </div>
+
+                            <div class="flex flex-col mt-4 px-10 mr-16 text-xs">
+                            <p class="mb-3 mr-4">Relate Link</p>
+                                <a href="https://stmi.ac.id/" class="flex mt-5">
+                                    <img src="{{ url('/images/images.png') }}" alt=""  class="h-14" >
+                                    <img src="{{ url('/images/download.png') }}" alt="" class="h-14">
+                                </a>
+                                
+                            </div>
+
+                            <div class="flex flex-col mt-4 px-10 mr-16 text-xs">
+                                <p class="mb-3 -mr-4">Sosial Media</p>
+                                <div class="flex flex-col mt-5 justify-between">
+                                <a href=""><img src="{{ url('/images/logo/download.png') }}" alt=""  class="w-10 h-10 mx-2" ></a>
+                                <a href=""><img src="{{ url('/images/logo/download (1).png') }}" alt="" class="w-10 h-10 mx-2 mt-2"></a>
+                                <a href=""><img src="{{ url('/images/logo/hd-meta-facebook-white-logo-sign-symbol-png-701751694777079kpf2z9a9s8.png') }}" alt="" class="w-10 h-10 mx-2 mt-2"></a>
+                                <a href=""><img src="{{ url('/images/logo/instagram.png') }}" alt="" class="w-10 h-10 mx-2 mt-2"></a>
+                                </div>
+                                <div class="flex w-12 h-8 mt-6 mr-3">
+
+                            
+                                <a href="https://stmi.ac.id/">
+                                    
+                                </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+</body>
+</html>
