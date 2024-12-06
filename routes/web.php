4bf88928e9b49912f3d1280c4e286d87518b2b06
peopleacc <accpeopleacc@gmail.com>
2024-12-06 19:38:34 +0700

    route ke halaman lecture sama userdata
    gw tambahin itu

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,14 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/lecture', function () {
+    return view('lecture');
+})->middleware(['auth', 'verified'])->name('lecture');
+
+Route::get('/userdata', function () {
+    return view('userdata');
+})->middleware(['auth', 'verified'])->name('userdata');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
