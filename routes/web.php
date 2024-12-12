c3412bc96209525c22c70b8ec986aa577d37083a
Adiyuw13 <144145040+ipangyuwono@users.noreply.github.com>
2024-12-12 08:20:18 +0700

    feat: add mycourse, dashboard mahasiswa, attempt quiz, mytask, and navbar

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,23 @@ Route::get('/studentdata', function () {
     return view('studentdata');
 })->middleware(['auth', 'verified'])->name('studentdata');
 
+Route::get('/calendar', function () {
+    return view('calendar');
+})->middleware(['auth', 'verified'])->name('calendar');
+
+Route::get('/mycourse', function () {
+    return view('mycourse');
+})->middleware(['auth', 'verified'])->name('mycourse');
+
+Route::get('/mytask', function () {
+    return view('mytask');
+})->middleware(['auth', 'verified'])->name('mytask');
+
+Route::get('/attempt', function () {
+    return view('attempt');
+})->middleware(['auth', 'verified'])->name('attempt');
+
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
