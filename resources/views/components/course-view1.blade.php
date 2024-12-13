e99e5836b7d1f323ba776369181797e0cb8f792b
peopleacc <accpeopleacc@gmail.com>
2024-12-13 19:51:50 +0700

    component buat mahasiswa di auth course v1 v2 auth

---
+++ resources/views/components/course-view1.blade.php
@@ -0,0 +1,41 @@
+<div class="flex mx-24 my-4 mb-24 px-10 ">
+    <div class="bg-white col-4 col-md-8 rounded shadow p-4 mx-4 w-1/4">
+        <img src="{{ url('/images/sisteminformasi.jpg') }}" alt="Sistem Informasi" class="w-full h-32 object-cover rounded mb-4">
+        <h3 class="text-sm font-semibold my-8">SISTEM INFORMASI</h3>
+         <div class="w-full bg-gray-200 mt-16 rounded-full h-2.5 my-2">
+            <div class="bg-blue-600 h-2.5 rounded-full" style="width: 0%;"></div>
+        </div>
+        <p class="text-xs font-medium text-gray-700 mb-4 q">0% Course Complete</p>
+        <a class="bg-blue-600 text-white px-4 py-2 ml-6 rounded mt-4" href="<?= url('coursefile'); ?>" >View Course</a>
+    </div>
+
+    <div class="bg-white rounded shadow p-4 mx-4 w-1/4">
+        <img src="{{ url('/images/basisdata.jpg') }}" alt="Sistem Informasi" class="w-full h-32 object-cover rounded mb-4">
+        <h3 class="text-sm font-semibold my-8">BASIS DATA</h3>
+        <div class="w-full bg-gray-200 mt-16 rounded-full h-2.5 my-2">
+            <div class="bg-blue-600 h-2.5 rounded-full" style="width: 0%;"></div>
+        </div>
+        <p class="text-xs font-medium text-gray-700 mb-4 q">0% Course Complete</p>
+        <a class="bg-blue-600 text-white px-4 py-2 ml-6 rounded mt-4" href="<?= url('coursefile'); ?>" >View Course</a>
+    </div>
+
+    <div class="bg-white rounded shadow p-4 mx-4 w-1/4">
+        <img src="{{ url('/images/risetoperasi.jpg') }}" alt="Sistem Informasi" class="w-full h-32 object-cover rounded mb-4">
+        <h3 class="text-sm font-semibold my-8">RISET OPERASI</h3>
+        <div class="w-full bg-gray-200 mt-16 rounded-full h-2.5 my-2">
+            <div class="bg-blue-600 h-2.5 rounded-full" style="width: 0%;"></div>
+        </div>
+        <p class="text-xs font-medium text-gray-700 mb-4 q">0% Course Complete</p>
+        <a class="bg-blue-600 text-white px-4 py-2 ml-6 rounded mt-4" href="<?= url('coursefile'); ?>" >View Course</a>
+    </div>
+    
+    <div class="bg-white rounded shadow p-4 mx-4 w-1/4">
+        <img src="{{ url('/images/algoritma.jpg') }}" alt="Sistem Informasi" class="w-full h-32 object-cover rounded mb-4">
+        <h3 class="text-sm font-semibold my-8">ALGORITMA & STRUKTUR </h3>
+        <div class="w-full bg-gray-200 mt-16 rounded-full h-2.5 my-2">
+            <div class="bg-blue-600 h-2.5 rounded-full" style="width: 0%;"></div>
+        </div>
+        <p class="text-xs font-medium text-gray-700 mb-4 q">0% Course Complete</p>
+        <a class="bg-blue-600 text-white px-4 py-2 ml-6 rounded mt-4" href="<?= url('coursefile'); ?>" >View Course</a>
+    </div>
+</div>
